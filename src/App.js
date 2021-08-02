ba2c129f571cda8a79f0fbdad03cc17f3b07adde
John <josephjohn727@gmail.com>
2021-08-02 18:49:07 -0400

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navfrom from "./components/Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <h1>My Portfolio ........</h1>;
+  return (
+    <div>
+      <Navbar />
+      {/* <h1>My Portfolio ........</h1>; */}
+    </div>
+  );
 }
 
 export default App;
